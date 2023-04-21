dd9d0d7d3d5eda8d3682727fd67ee5f509ffc892
Kangrui Liu <58324716+krliu67@users.noreply.github.com>
2023-04-21 09:41:39 +0800

    Update getChildList.sql

--- getChildList.sql
+++ getChildList.sql
@@ -3,8 +3,8 @@ FUNCTION `getChildList`(rootId VARCHAR(100)) RETURNS varchar(1000) CHARSET utf8
 BEGIN
 /*
 1：pk => id
-2：table name => course_category
-3：father => parentid
+2：table name => table_name
+3：father => parent_id
 */
     DECLARE sTemp VARCHAR(1000);
     DECLARE sTempChd VARCHAR(1000);
@@ -14,7 +14,7 @@ BEGIN
  
     WHILE sTempChd is not null DO
         SET sTemp = concat(sTemp,',',sTempChd);
-        SELECT group_concat(id) INTO sTempChd FROM course_category where FIND_IN_SET(parentid,sTempChd)>0;
+        SELECT group_concat(id) INTO sTempChd FROM table_name where FIND_IN_SET(parent_id,sTempChd)>0;
     END WHILE;
     RETURN sTemp; 
 END
